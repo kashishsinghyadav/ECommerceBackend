61649f98e828df3f8befce4877fe1cbbc8110945
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-03-25 22:31:17 +0530

    add productcontroller

--- src/main/java/org/kashish/ecommerce/controller/productController.java
+++ src/main/java/org/kashish/ecommerce/controller/productController.java
@@ -4,21 +4,19 @@ package org.kashish.ecommerce.controller;
 import org.kashish.ecommerce.model.Product;
 import org.kashish.ecommerce.service.productService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class productController {
 
     @Autowired
     private productService productservice;
 
-    @GetMapping("/product")
+    @GetMapping("/products")
     public List<Product> getProduct() {
         return productservice.getAllProduct();
 
