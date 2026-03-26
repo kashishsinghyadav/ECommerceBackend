a8740fab7ab1995095f473f05c4a70b87523a11c
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-03-26 17:10:59 +0530

    add image related datatype

--- src/main/java/org/kashish/ecommerce/model/Product.java
+++ src/main/java/org/kashish/ecommerce/model/Product.java
@@ -2,10 +2,7 @@ package org.kashish.ecommerce.model;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -33,5 +30,11 @@ public class Product {
     private Date releaseDate;
     private boolean productAvailable;
     private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
+
+
 
 }
