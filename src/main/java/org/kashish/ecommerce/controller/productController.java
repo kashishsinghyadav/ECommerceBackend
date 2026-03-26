c3913672f915875c53418bc19e9684e3ca195300
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-03-26 18:11:50 +0530

    handle the image data from frontend store in multipart

--- src/main/java/org/kashish/ecommerce/controller/productController.java
+++ src/main/java/org/kashish/ecommerce/controller/productController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -34,6 +36,20 @@ public class productController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
 
+
+    }
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,@RequestPart MultipartFile imageFile) {
+
+        Product product1= null;
+        try {
+            product1 = productservice.addProduct(product,imageFile);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+        return new ResponseEntity<>(product1,HttpStatus.CREATED);
 
     }
 
--- src/main/java/org/kashish/ecommerce/service/productService.java
+++ src/main/java/org/kashish/ecommerce/service/productService.java
@@ -4,7 +4,9 @@ import org.kashish.ecommerce.model.Product;
 import org.kashish.ecommerce.repo.productRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -21,5 +23,12 @@ public class productService {
 
     public Product getProductById(int id) {
         return productRepo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageData(imageFile.getBytes());
+        product.setImageType(imageFile.getContentType());
+        return productRepo.save(product);
     }
 }
