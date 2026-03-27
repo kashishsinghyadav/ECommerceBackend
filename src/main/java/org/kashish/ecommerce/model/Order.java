381db1ab11948bce361539c9f330e3dc91b1b780
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-03-27 16:35:03 +0530

    add ordermodel in models

---
+++ src/main/java/org/kashish/ecommerce/model/Order.java
@@ -0,0 +1,32 @@
+package org.kashish.ecommerce.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Order {
+
+    @Id
+    private Long id;
+    @Column(unique = true)
+    private String orderId;
+    private String customerName;
+    private String email;
+    private String status;
+    private LocalDate orderDate;
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems;
+}
---
+++ src/main/java/org/kashish/ecommerce/model/OrderItem.java
@@ -0,0 +1,31 @@
+package org.kashish.ecommerce.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    @Id
+    private Integer id;
+
+    @ManyToOne
+    private Product product;
+    private int quantity;
+
+    private BigDecimal totalPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
+}
