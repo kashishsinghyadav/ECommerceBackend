3d8af659fcf15502d318da26b8944e3e142025e1
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-03-30 14:39:33 +0530

    add logger

--- src/main/java/org/kashish/ecommerce/aop/LoggerAspect.java
+++ src/main/java/org/kashish/ecommerce/aop/LoggerAspect.java
@@ -1,5 +1,21 @@
 package org.kashish.ecommerce.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Aspect
 public class LoggerAspect {
 
+
+    private  static final Logger LOGGER = LoggerFactory.getLogger(LoggerAspect.class);
+
+//    @Before("execution(* *.*(..))")
+    public void log() {
+        LOGGER.info("logger info");
+    }
 }
